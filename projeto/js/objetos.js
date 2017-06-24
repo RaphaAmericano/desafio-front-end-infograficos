cd0db60d2dea28881b0f8dee44e93afa2a9a975e
RaphaAmericano <raphael@raphaelamericano.com.br>
2017-06-24 20:16:28 -0300

    Funcionalidades

--- projeto/js/objetos.js
+++ projeto/js/objetos.js
@@ -1,6 +1,7 @@
 function Banco(link, slide = false){
 
 	var banco_de_dados;
+	var ArrDatas = [];
 	this.link_banco = link;
 	// this.banco_de_dados = [];
 	this.slideStatus = slide;
@@ -26,10 +27,19 @@ function Banco(link, slide = false){
 		};
 		requisicaoNoticias.send(null);	
 	};
+	//Função de auxilio para desenvolvimento - inutil par pratica
 	this.mostrarBanco = function(){
-		//alert(banco_de_dados);
-		return banco_de_dados[0];
+		if(this.slideStatus === true){
+			return banco_de_dados[0];
+		}else {
+			return banco_de_dados[0]['Editorias'][0]['Notícias'][0];
+		}
 	}
+	this.mostrarArrData = function(){
+		return ArrDatas;
+	}
+
+	//
 	this.displayMaterias = function(){
 		if( this.slideStatus === false){
 			var htmlNoticias = '';
@@ -89,7 +99,91 @@ function Banco(link, slide = false){
 				return;
 			}	
 	}//Fim display Materias
+	this.mudarFormatoData = function (){
+		var dataNova;
+		for(i = 0; i < banco_de_dados[0]['Editorias'].length; i++){
+			for(j = 0; j < banco_de_dados[0]['Editorias'][i]['Notícias'].length; j++){
 
+			dataNova = banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Data de publicação"];
+			dataNova = dataNova.split('-');
+			
+			d = new Date(dataNova[2],dataNova[1], dataNova[0] );
+
+			banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Data de publicação"] = d;
+			}
+		}
+	}
+	this.montarArrayDatas = function(){
+		for(i = 0; i < banco_de_dados[0]['Editorias'].length; i++){
+			for(j = 0; j < banco_de_dados[0]['Editorias'][i]['Notícias'].length; j++){
+				ArrDatas.push( banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Data de publicação"]);			
+				}
+			}
+			ArrDatas.sort(function(a, b){
+				return a - b; 
+		});
+	}
+	this.organizarPorData = function(){
+		var htmlNoticias = '';
+		var htmlComboBoxOrdenar = '';
+		var htmlComboBoxFiltrar = '';
+		var divEditoriais = document.getElementById("editoriais");
+		var selectBoxEditoria = document.getElementById("filtrarEditoria");
+
+		while(ArrDatas.pop() !==  undefined){
+			for(i = 0; i < banco_de_dados[0]["Editorias"].length; i++){
+				for(j = 0; j < banco_de_dados[0]['Editorias'][i]['Notícias'].length; j++){		
+					for(k = 0; k < ArrDatas.length; k++){
+						if(banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Data de publicação"] === ArrDatas[k]){
+							alert(i + " "+j+" "+k );
+							// alert(ArrDatas[k]);
+							//ArrDatas.pop();
+							htmlNoticias += '<div class="thumbnail">';
+							htmlNoticias += '<span class="data_publicacao">'+banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Data de publicação"]+'</span>';
+							htmlNoticias += "<h5><b>"+banco_de_dados[0]['Editorias'][i]['Editoria']+"</b></h5>";
+							htmlNoticias += '<img class="thumb_img" src="../Arquivos/Imagens/Notícias/'+banco_de_dados[0]['Editorias'][i]['Notícias'][j]['Foto']+'">';
+							htmlNoticias += "<h4>"+banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Título"]+"</h4>";
+							htmlNoticias += '<p class="thumb_paragrafo">'+banco_de_dados[0]['Editorias'][i]['Notícias'][j]["Texto"]+'</p>';					
+							htmlNoticias += '</div>';	
+							break;
+						}
+					}
+				}//for j
+			}//for i
+		 }
+		 divEditoriais.insertAdjacentHTML('beforeend', htmlNoticias);
+		for( i = 0; i < banco_de_dados[0]['Editorias'].length; i++){
+				htmlComboBoxOrdenar += '<option value='+i+'>'+banco_de_dados[0]['Editorias'][i]['Editoria']+'</option>';
+		}
+		selectBoxEditoria.insertAdjacentHTML("beforeend", htmlComboBoxOrdenar);
+		htmlComboBoxOrdenar += '<option value='+i+'>'+banco_de_dados[0][1]+'</option>';
+	}
 }
+
+
+//Objeto[0].Editorias[0].Notícias[0]["Data de publicação"]
 var linkJSONnoticias = '../Arquivos/JSON/noticias.json';
+var linkJSONslides = '../Arquivos/JSON/slide.json';
 var teste = new Banco(linkJSONnoticias);
+var testeSlides = new Banco(linkJSONslides, true);
+var testeData = new Banco(linkJSONnoticias);
+testeData.iniciarBanco();
+testeData.mudarFormatoData();
+testeData.montarArrayDatas();
+testeData.displayMaterias();
+testeData.mostrarBanco();
+
+
+// var ArrTest = [
+//   {
+//     "Nome": "Notícias",
+//     "Editorias": [
+//     	{
+//         "Editoria": "Esporte",
+//         "Id": "574c58853ecb94fd25aeb8a8",
+//         "Notícias": [
+//           {
+//             "Foto": "3.jpg",
+//             "Título": "Consumo de chocolate e depressão estão associados.",
+//             "Texto": "Sint dolor consequat occaecat non aliqua non adipisicing id. Lorem Lorem anim quis incididunt laboris consequat sunt id. Ad laborum magna ad non do dolor. Tempor sunt et eiusmod consequat. Cupidatat labore incididunt commodo nisi elit id eu commodo culpa Lorem Lorem veniam. Id esse mollit qui culpa aliquip aliqua esse adipisicing anim ex magna. Et ipsum sint ad nisi esse sunt labore laboris voluptate dolor ut.\r\n",
+//             "Data de publicação": "06-01-2015"}]}]}];
